295996faadde5f968d964522934455ec8326c842
hhdsjgit <abc18688567431@163.com>
2026-09-22 23:05:05 +0800

    修复玩偶没掉落问题

--- src/main/java/net/hhdsj/changed_creatures/block/LatexDeepCrystalDragonDollBlock.java
+++ src/main/java/net/hhdsj/changed_creatures/block/LatexDeepCrystalDragonDollBlock.java
@@ -2,15 +2,19 @@ package net.hhdsj.changed_creatures.block;
 
 import net.foxyas.changedaddon.block.AbstractPlushyBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class LatexDeepCrystalDragonDollBlock extends AbstractPlushyBlock {
 
