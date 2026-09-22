c8bb65a0c20fd5ef909f5e924119913c13f0ebfc
hhdsjgit <abc18688567431@163.com>
2026-09-22 22:46:32 +0800

    修复移速如同闪电侠的问题

--- src/main/java/net/hhdsj/changed_creatures/entity/simple/LatexCrystalJellyDragonEntity.java
+++ src/main/java/net/hhdsj/changed_creatures/entity/simple/LatexCrystalJellyDragonEntity.java
@@ -57,7 +57,6 @@ public class LatexCrystalJellyDragonEntity extends ChangedEntity {
     public static AttributeSupplier.Builder createAttributes() {
         return Mob.createMobAttributes()
                 .add(Attributes.MAX_HEALTH, 80.0D)
-                .add(Attributes.MOVEMENT_SPEED, 0.1D)
                 .add(Attributes.FOLLOW_RANGE, 20.0D)
                 .add(Attributes.KNOCKBACK_RESISTANCE, 0.6D)
                 .add(Attributes.ARMOR, 4.0D);
