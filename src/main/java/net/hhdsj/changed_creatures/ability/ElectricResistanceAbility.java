8341285ba8370b65f15aeb282f33ddc41a13f065
hhdsjgit <abc18688567431@163.com>
2026-09-25 22:30:06 +0800

    添加2个新能力awa

--- src/main/java/net/hhdsj/changed_creatures/ability/ElectricResistanceAbility.java
+++ src/main/java/net/hhdsj/changed_creatures/ability/ElectricResistanceAbility.java
@@ -1,13 +1,15 @@
 package net.hhdsj.changed_creatures.ability;
 
+import net.hhdsj.changed_creatures.ChangedCreature;
 import net.hhdsj.changed_creatures.ability.data.AbstractAbility;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 
 import java.util.List;
 
 public class ElectricResistanceAbility extends AbstractAbility {
-
+    private final ResourceLocation texture = ResourceLocation.fromNamespaceAndPath(ChangedCreature.MODID,"textures/gui/ability/latex_ability_anit_lightning.png");
     public ElectricResistanceAbility() {
         super("◈ 抗电", 0xFFFF88, 5);
     }
@@ -20,5 +22,8 @@ public class ElectricResistanceAbility extends AbstractAbility {
     public void appendHoverText(List<Component> list, Player player, int level) {
         list.add(Component.literal("减少电击的持续时间"));
     }
-
+    @Override
+    public ResourceLocation getAbilityTexture() {
+        return texture;
+    }
 }
